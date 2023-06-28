e4293ee22485e7a00d48bcec99b5b11d99fb2741
Youth-49 <youth.049@gmail.com>
2023-06-28 14:03:17 +0800

    write readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # 2023-DOS-IN
+
+
+
+to reproduce the Figure 6,7,8,9,10,11:
+
+```shell
+cd synthetic_data
+python DOS-IN-plot.py
+```
+
+
+
+to reproduce the Figure 12:
+
+```shell
+cd sensitive
+sh run.sh
+cd res
+python comp.py
+```
+
+
+
+to test DOS-IN performance and runtime in real world datasets:
+
+```sh
+cd real_data
+sh run_opt.sh # for faster DOS-IN
+```
+
+
+
+to reproduce the outlier detection results:
+
+```shell
+cd outlier
+python DOS-IN-plot.py
+```
+
---
+++ sensitive/run.sh
@@ -0,0 +1,50 @@
+python DOS-IN.py --dataset R15.tsv
+python DOS.py --dataset R15.tsv
+
+python DOS-IN.py --dataset D31.txt
+python DOS.py --dataset D31.txt
+
+python DOS-IN.py --dataset g2-2-20.tsv
+python DOS.py --dataset g2-2-20.tsv
+
+python DOS-IN.py --dataset breastTissue.txt
+python DOS.py --dataset breastTissue.txt
+
+python DOS-IN.py --dataset divorce.txt
+python DOS.py --dataset divorce.txt
+
+python DOS-IN.py --dataset iris.txt
+python DOS.py --dataset iris.txt
+
+python DOS-IN.py --dataset wdbc.txt
+python DOS.py --dataset wdbc.txt
+
+python DOS-IN.py --dataset wine.txt
+python DOS.py --dataset wine.txt
+
+python DOS-IN.py --dataset skewed.txt
+python DOS.py --dataset skewed.txt
+
+python DOS-IN.py --dataset asymmetric.txt
+python DOS.py --dataset asymmetric.txt
+
+python DOS-IN.py --dataset digits.txt
+python DOS.py --dataset digits.txt
+
+python DOS-IN.py --dataset ecoli.txt
+python DOS.py --dataset ecoli.txt
+
+python DOS-IN.py --dataset zoo.txt
+python DOS.py --dataset zoo.txt
+
+python DOS-IN.py --dataset lenses.txt
+python DOS.py --dataset lenses.txt
+
+python DOS-IN.py --dataset led7digit.txt
+python DOS.py --dataset led7digit.txt
+
+python DOS-IN.py --dataset fertility_Diagnosis.txt
+python DOS.py --dataset fertility_Diagnosis.txt
+
+python DOS-IN.py --dataset Spiral.tsv
+python DOS.py --dataset Spiral.tsv
